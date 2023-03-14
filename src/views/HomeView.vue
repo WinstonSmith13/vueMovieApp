c581b404e296c6070216ed6b5f124138295b0b67
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-14 16:41:01 +0100

    feat: working on the architecture of the front end.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid justify-center">
+    <input 
+          class="text-white px-4 py-1 placeholder-blue-300 border bg-gray-600 border-blue-500 rounded outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+          placeholder="Que cherches-tu ?" />
+
+          <button class="bg-yellow-300 rounded-lg mt-4 hover:bg-yellow-50">SEARCH</button>
+        </div>
+
 </template>
+
+<script setup>
+
+</script>
