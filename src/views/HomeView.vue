ae27ec8673bd4b4b1128d6fb4fbf83e83ed33c90
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-14 19:18:34 +0100

    feat: working on the fetch and search feature.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="grid justify-center">
     <input 
+          v-model="search"
           class="text-white px-4 py-1 placeholder-blue-300 border bg-gray-600 border-blue-500 rounded outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
           placeholder="Que cherches-tu ?" />
 
-          <button class="bg-yellow-300 rounded-lg mt-4 hover:bg-yellow-50">SEARCH</button>
+          <button @click="getApi" class="bg-yellow-300 rounded-lg mt-4 hover:bg-yellow-50">SEARCH</button>
         </div>
 
 </template>
 
-<script setup>
+<script>
+export default {
+  data(){
+    return {
+      search: ''
+    }
+  },
+methods:{
+  getApi(){
+    console.log(this.search)
+  fetch(`http://www.omdbapi.com/?s=${this.search}&apikey=9630de31`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+}
+}
 
 </script>
