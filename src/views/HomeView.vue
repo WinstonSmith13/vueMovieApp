dd197c13193953b84b8ef93a7789b5749b67b940
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-15 13:12:31 +0100

    api: working on saving data from omdb and display them.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="grid justify-center">
-    <input 
-          v-model="search"
-          class="text-white px-4 py-1 placeholder-blue-300 border bg-gray-600 border-blue-500 rounded outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
-          placeholder="Que cherches-tu ?" />
+    <input v-model="search"
+      class="text-white px-4 py-1 placeholder-blue-300 border bg-gray-600 border-blue-500 rounded outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+      placeholder="Que cherches-tu ?" />
 
-          <button @click="getApi" class="bg-yellow-300 rounded-lg mt-4 hover:bg-yellow-50">SEARCH</button>
-        </div>
-
+    <button @click="getApi" class="bg-yellow-300 rounded-lg mt-4 hover:bg-yellow-50">SEARCH</button>
+  </div>
 </template>
 
-<script>
-export default {
-  data(){
+<script setup>
+import { ref } from "vue";
+
+//State
+const search = ref('');
+
+//methods
+
+function getApi() {
+  console.log(search.value)
+  fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=9630de31`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+
+
+/*   data(){
     return {
       search: ''
     }
@@ -24,7 +37,7 @@ methods:{
   .then(response => response.json())
   .then(data => console.log(data))
 }
-}
-}
+} */
+
 
 </script>
