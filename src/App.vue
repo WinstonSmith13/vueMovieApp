617f98fa896f44cfa6a56213973b14e6783e9c36
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-17 14:09:50 +0100

    feat: working on emits.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <router-link class="text-white grid justify-center pt-10  font-bold" to="/">
       <h1><span class="text-yellow-400 font-medium">Vue</span>Movies</h1>
     </router-link>
+    <router-link class="text-white grid justify-center  font-bold" to="/about">
+      <h1><span class="text-yellow-400 font-medium">About</span></h1>
+    </router-link>
   </header>
   <main class="h-full w-full">
     <router-view />
